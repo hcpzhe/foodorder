77f54c472474ecca7be0210e5e6a25fd63b80af6
hcpzhe <zheyun999@163.com>
2014-09-25 18:14:34 +0800

    manage/StoreController update

--- Application/Manage/Controller/StoreController.class.php
+++ Application/Manage/Controller/StoreController.class.php
@@ -76,6 +76,21 @@ class StoreController extends ManageBaseController {
 		$this->display();
 	}
 	
+	public function update() {
+		$id = (int)I('id');
+		if ($id <= 0) {
+			$this->error('请选择要更新的店铺');
+		}
+		$model = New StoreModel();
+		if (false === $model->create($_POST,Model::MODEL_UPDATE)) {
+			$this->error($model->getError());
+		}
+		if (false === $model->where('`id`='.$id)->save()) {
+			$this->error($model->getError());
+		}
+		$this->success('更新成功',C('CURRENT_URL_NAME'));
+	}
+	
 	public function add() {
 		cookie(C('CURRENT_URL_NAME'),$_SERVER['REQUEST_URI']);
 		$this->display();
@@ -90,5 +105,43 @@ class StoreController extends ManageBaseController {
 			$this->error($model->getError());
 		}
 		$this->success('新建成功',U(CONTROLLER_NAME.'/lists'));
+	}
+	
+	/**
+	 * 删除接口, set status=-1
+	 */
+	public function del() {
+		$id = (int)I('id');
+		if ($id <= 0) {
+			$this->error('请选择要删除的店铺');
+		}
+		$model = New Model('Store');
+		if (false === $model->where('`id`='.$id)->setField('status','-1')) {
+			$this->error('删除失败,未知错误!');
+		}
+		$this->success('删除成功');
+	}
+	
+	/**
+	 * 状态的切换
+	 */
+	public function toggle() {
+		$fields = array('recom'=>'is_recom','close'=>'is_close');
+		$id = (int)I('id');
+		if ($id <= 0) {
+			$this->error('主要参数非法');
+		}
+		$field_key = I('f');
+		if (!key_exists($field_key, $fields)) {
+			$this->error('参数非法');
+		}
+		$model = New Model('Store');
+		$map = array('id' => $id);
+		$org = $model->where($map)->getField($fields[$field_key]);
+		$new = $org==='1' ? '0' : '1';
+		if (false === $model->where($map)->setField($fields[$field_key],$new)) {
+			$this->error('更新失败,未知错误!');
+		}
+		$this->success('更新成功');
 	}
 }
