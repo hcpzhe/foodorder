876789387c792f771ad74f836dc91fa77df365b1
hcpzhe <zheyun999@163.com>
2014-09-26 11:13:26 +0800

    manage/AttrModel update add static $ablemap & func findAble()

--- Application/Manage/Model/AttrModel.class.php
+++ Application/Manage/Model/AttrModel.class.php
@@ -6,6 +6,8 @@ use Think\Model;
  * 筛选属性
  */
 class AttrModel extends Model {
+	public static $status = array('del'=>'-1','forbid'=>'0','allow'=>'1');
+	public static $ablemap = array('status'=>'1'); //状态正常的查询条件
 
 	/**
 	 * MUST_VALIDATE	必须验证 不管表单是否有设置该字段
@@ -21,6 +23,22 @@ class AttrModel extends Model {
     		array('sort',array('0','99999'),'排序值非法',self::EXISTS_VALIDATE,'between'),
 			array('status',array('-1','0','1'),'属性状态非法',self::EXISTS_VALIDATE,'in'),//-1-删除 0-禁用 1-正常
 	);
-	
+
+	/**
+	 * 查找 状态正常 的属性
+	 * @param PKID或者array $options
+	 * @return array
+	 */
+	public function findAble($options=array()) {
+		$where = array();
+		if (is_numeric($options) || is_string($options)) {
+			$where[$this->getPk()]  =   $options;
+		}else {
+			$where = $options;
+		}
+		
+		if ($where['status']===null||$where['status']===''||!in_array($where['status'],self::$status)) $where['status'] = '1';
+		return $this->where($where)->find();
+	}
 	
 }
