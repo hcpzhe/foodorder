a54c6a0e7d5d898a9e1cef56aa49d7042e7525a4
hcpzhe <zheyun999@163.com>
2014-11-07 16:20:17 +0800

    manage/Config 网站设置

--- Application/Common/Common/function.php
+++ Application/Common/Common/function.php
@@ -1,6 +1,19 @@
 <?php
 //公共函数
-
+// 分析枚举类型配置值 格式 a:名称1,b:名称2
+function parse_config_attr($string) {
+	$array = preg_split('/[,;\r\n]+/', trim($string, ",;\r\n"));
+	if(strpos($string,':')){
+		$value  =   array();
+		foreach ($array as $val) {
+			list($k, $v) = explode(':', $val);
+			$value[$k]   = $v;
+		}
+	}else{
+		$value  =   $array;
+	}
+	return $value;
+}
 /**
  * 检测用户是否登录
  * @return integer 0-未登录，大于0-当前登录用户ID
--- Application/Manage/Controller/ConfigController.class.php
+++ Application/Manage/Controller/ConfigController.class.php
@@ -1,6 +1,7 @@
 <?php
 namespace Manage\Controller;
 use Common\Controller\ManageBaseController;
+use Manage\Model\ConfigModel;
 
 /**
  * 系统配置
@@ -9,4 +10,25 @@ use Common\Controller\ManageBaseController;
  */
 class ConfigController extends ManageBaseController {
 	
+	/**
+	 * 按分组查看
+	 * @param int $gid	group分组编号
+	 */
+	public function group($gid=null) {
+		$groups = ConfigModel::$group;
+		$this->assign('groups',$groups); //分组的数组
+		
+		$map = ConfigModel::$ablemap; //基础查询条件
+		if (array_key_exists($gid, ConfigModel::$group)) {
+			$map['group'] = $gid;
+		}else {
+			reset($groups);
+			$map['group'] = $gid = key($groups);
+		}
+		$model = new ConfigModel();
+		$list = $model->where($map)->order('sort asc, id desc')->select();
+		$this->assign('list',$list); //配置数组
+		$this->assign('nowgid',$map['group']); //当前的分组id
+		$this->display();
+	}
 }
