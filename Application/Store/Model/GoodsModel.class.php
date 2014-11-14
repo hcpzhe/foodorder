008472827239b5915d723d85b63c74775cbabf46
hcpzhe <zheyun999@163.com>
2014-11-14 11:57:49 +0800

    store/GoodsModel 店铺商品模型

---
+++ Application/Store/Model/GoodsModel.class.php
@@ -0,0 +1,19 @@
+<?php
+namespace Store\Model;
+use Think\Model;
+
+/**
+ * 商品模型
+ */
+class GoodsModel extends \Manage\Model\GoodsModel {
+	
+	/**
+	 * 判断商品, 是否所属于自己的店铺
+	 * @param int $goods_id		商品主键id
+	 * @return boolean
+	 */
+	public function checkAuth($goods_id) {
+		$store_id = $this->where("`id`=$goods_id")->getField('store_id');
+		return ($store_id == SID);
+	}
+}
