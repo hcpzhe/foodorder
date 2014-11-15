7ae152e601322229c41badc1436e43f66363f9cc
hcpzhe <zheyun999@163.com>
2014-11-16 04:11:43 +0800

    Store/View 自动frame高度,修复; 弹窗与frame高度放入通用js文件

--- Public/js/store.content.js
+++ Public/js/store.content.js
@@ -1,13 +1,29 @@
 //Store module content page inculde this file
+var org_height , new_height;
 $(document).ready(function() {
 	$(document).on("mousemove change",function(){
 		$(parent.document).find("#content_frame").height($("body").height());
+	});
+
+	$('.modal').on('shown.bs.modal', function (e) {
+		org_height = document.body.scrollHeight;
+		new_height = $('.modal-backdrop').height();
+		if (org_height < new_height) {
+			$("body").height(new_height);
+			$(parent.document).find("#content_frame").height(new_height);
+		}
+	});
+	$('.modal').on('hidden.bs.modal', function (e) {
+		if (org_height < new_height) {
+			$("body").height(org_height);
+			$(parent.document).find("#content_frame").height(org_height);
+		}
 	});
 	
 	//RSsunmit 免跳转处理
 	$(".bat_a_handle").click(function() {
 		if ($(this).hasClass("RSconfirm")) {
-			if(confirm("确定要清空数据吗？")) {
+			if(confirm("确定要进行此项操作吗？")) {
 				var data = {success_alert:false,reload:true};
 				if ($(this).hasClass("RSalert")) data.success_alert = true;
 				$(this).RSsubmit(data);
