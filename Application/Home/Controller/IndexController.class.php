1b3ae1c5878c388d613c56b5fba5ddaf4c848d22
hcpzhe <zheyun999@163.com>
2014-11-18 17:58:37 +0800

    Home/View 首页 完成

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -1,10 +1,16 @@
 <?php
 namespace Home\Controller;
 use Common\Controller\HomeBaseController;
+use Think\Model;
 
 class IndexController extends HomeBaseController {
 
 	public function index() {
+		//筛选属性id为1的所有属性值
+		$attrval_M = new Model('AttrVal');
+		$attrval = $attrval_M->where("attr_id=1 AND status=1")->order("sort asc,id desc")->getField("id,attr_val");
+		$this->assign('attrval',$attrval);//筛选分类
+		
 		$this->display();
 	}
 	
