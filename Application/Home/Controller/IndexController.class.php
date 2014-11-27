c30b80aa90e72943d6e57aa2d4b1b1dbc31966be
hcpzhe <zheyun999@163.com>
2014-11-27 22:13:49 +0800

    Home/ 购物车页面

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -17,4 +17,16 @@ class IndexController extends HomeBaseController {
 	public function welcome() {
 		$this->display();
 	}
+	
+	public function cart() {
+		$this->assign('list','123');
+		$this->display();
+	}
+	
+	/**
+	 * 确认下单页面
+	 */
+	public function confirm() {
+		$this->display();
+	}
 }
